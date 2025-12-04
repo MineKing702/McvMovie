41b73d6917fc00778a85c7d6da1a302345ca765e
Andrew G <gountisa@roverkids.org>
2025-12-04 12:09:58 -0500

    Added a search and a rating field

--- McvMovie/Migrations/20251204170118_Rating.Designer.cs
+++ McvMovie/Migrations/20251204170118_Rating.Designer.cs
@@ -4,6 +4,7 @@ using McvMovie.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace McvMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class McvMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20251204170118_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
